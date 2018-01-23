3d31116588fcf80470c0037a8748550ab08d1b81
TaylorPratt <tppratt@gmail.com>
2018-01-22 22:41:22 -0500

    Update scriptPixel.js

--- scriptPixel.js
+++ scriptPixel.js
@@ -1,11 +1,8 @@
-function setString (p1,p2)
-{
   var query = location.search.substring(1);
 // Pull the URL query string.
   var p1 = query.slice(0,query.search("="));
 // Set p1 to the first parameter of the query string. 
   var p2 = query.slice(query.search("=")+1);
 // Set p2 to the first parameter of the query string. 
-}
-  "http://i.w55c.net/a.gif?p1=" +p1 "&p2=" +p2
-  http://i.w55c.net/b.gif?price=500&p2={p2_parsed_value}&sku=d90&p1={p1_parsed_va
+("http://i.w55c.net/a.gif?p1=" +p1 "&p2=" +p2)
+("http://i.w55c.net/b.gif?price=500&p2=" +p2 "&sku=d90&p1=" +p1)
