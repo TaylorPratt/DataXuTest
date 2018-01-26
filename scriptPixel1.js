9ea4272600f642804310780eaf87c56721db3dc7
TaylorPratt <tppratt@gmail.com>
2018-01-25 20:18:34 -0500

    Update and rename scriptPixel.js to scriptPixel1.js

---
+++ scriptPixel1.js
@@ -0,0 +1,9 @@
+var query = location.search.substring(1);
+// Pull the URL query string.
+var p1 = query.slice(0,query.search("="));
+// Set p1 to the first parameter of the query string.
+var p2 = query.slice(query.search("=")+1);
+// Set p2 to the  parameter of the query string. -->
+document.getElementById("Pixel").src = "http://i.w55c.net/a.gif?p1=" +p1 "&p2=" +p2)  
+
+("http://i.w55c.net/b.gif?price=500&p2=" +p2 "&sku=d90&p1=" +p1)
